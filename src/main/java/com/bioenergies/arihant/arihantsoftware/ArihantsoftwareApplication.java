bc69e2cd9aaf4fc7cbe1b7274309685bb31fab66
Prashant Jain <prashant060794@gmail.com>
2020-09-29 13:53:19 +0530

    arihantsoftware

---
+++ src/main/java/com/bioenergies/arihant/arihantsoftware/ArihantsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.bioenergies.arihant.arihantsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArihantsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArihantsoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bioenergies/arihant/arihantsoftware/ArihantsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bioenergies.arihant.arihantsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArihantsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
